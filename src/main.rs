c65b89635c7aaaeaa1e7269cc2f3b58ddec7d650
[Cody Williams] <[me@codythedoer.com]>
2024-10-02 15:25:54 -0400

    Building user interface match conditions

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@
 // Then let the user retrieve a list of all people in a department
 // or all people in the company by department, sorted alphabetically.
 
+use std::io;
 use std::collections::HashMap;
 
 fn main() {
     println!("Welcome to the Bigsby Department Manager Interface.");
     
-    // Gen HashMap 
+    // Gen HashMap and departments
     let mut company_department_map: HashMap<String, Vec<String>> = HashMap::new(); 
     let dept_index: Vec <&str> = [
         "Sales", 
@@ -19,8 +20,7 @@ fn main() {
         "Shipping"
     ].to_vec();
     
-
-    // Gen data
+    // Gen current employee data
     let dept_employees_sales: Vec <&str> = [
         "Billy Bigsby", 
         "Marty Bigsby", 
@@ -53,31 +53,69 @@ fn main() {
     company_department_map = add_emp_batch_to_dept(company_department_map.clone(), dept_index.clone(), 3, dept_employees_maintenance);
     company_department_map = add_emp_batch_to_dept(company_department_map.clone(), dept_index.clone(), 4, dept_employees_shipping);
 
-    // Print data for Review    
-    all_department_print(company_department_map.clone(), dept_index);
+    println!("What would you like to do?");
+    println!("Example Actions: ");
+    println!("   Add 'Employee' to 'Department'");
+    println!("   Review employees in 'Department'");
+    println!("   Review all employees");
+
+    let mut user_entry = Vec::new();
+    manual_entry(&mut user_entry);
+
+    let parsed_user_entry = split_string_on_space_return_multi_part_vec(user_entry[0].clone());
+    println!("parsed_user_entry: {:?}", parsed_user_entry);    
+    
+    // User interface
+    match parsed_user_entry[0].trim() {
+        "add" => {
+            println!("Add");
+        },
+        "review" => {
+            if parsed_user_entry[1].trim() == "all" {
+                println!("Review all");
+            } else {
+                println!("Review");
+            }
+        },
+        &_ => {
+            println!("Failed");
+        }
+    }
+
+    // // Print data for Review    
+    // all_department_print(company_department_map.clone(), dept_index);
 }
+
+fn split_string_on_space_return_multi_part_vec(s: String) -> Vec<String> {
+    let parts: Vec<String> = s
+        .split(" ")
+        .map(|part| part.to_string().to_lowercase())
+        .collect();
+    parts
+}
+
 
 fn add_emp_batch_to_dept(mut company_department_map: HashMap<String, Vec<String>>, dept_index: Vec<&str>, dept: usize, employees: Vec<&str>) -> HashMap<String, Vec<String>> {
     for employee in employees {
-        company_department_map = add_emp_to_dept(company_department_map.clone(), &dept_index[dept], &employee.to_string());
+        company_department_map = add_emp_to_dept(company_department_map.clone(), &dept_index[dept], employee.to_string());
     }
 
     company_department_map
 }
 
-fn add_emp_to_dept(mut hashmap: HashMap<String, Vec<String>>, dept: &str, employee: &String) -> HashMap<String, Vec<String>> {
-    hashmap.entry(dept.to_string()
+fn add_emp_to_dept(mut company_department_map: HashMap<String, Vec<String>>, dept: &str, employee: String) -> HashMap<String, Vec<String>> {
+    company_department_map.entry(dept.to_string()
         .to_string())
         .or_insert_with(Vec::new)
         .push(format!("{employee}"));
 
-    hashmap
+    company_department_map
 }
 
-fn sort_employees(hashmap: HashMap<String, Vec<String>>, target_department: Vec<&str>, index: u64) -> Vec<String> {
+fn sort_employees(company_department_map: HashMap<String, Vec<String>>, target_department: Vec<&str>, index: u64) -> Vec<String> {
     let mut employees_sorted: Vec<String> = Vec::new();
     
-    for (department, employees) in hashmap {
+    for (department, employees) in company_department_map {
         if department == target_department[index as usize] {
             for employee in employees {
                 employees_sorted.push(employee.to_string());
@@ -89,44 +127,44 @@ fn sort_employees(hashmap: HashMap<String, Vec<String>>, target_department: Vec<
     employees_sorted
 }
 
-fn department_print(hashmap: HashMap<String, Vec<String>>, target_department: Vec<&str>, index: u64) {
+fn department_print(company_department_map: HashMap<String, Vec<String>>, target_department: Vec<&str>, index: u64) {
     println!("dept: {:?}", target_department[index as usize]);    
-    let sorted_employees = sort_employees(hashmap, target_department, index);
+    let sorted_employees = sort_employees(company_department_map, target_department, index);
     println!("sorted_employees: {:?}", sorted_employees);
 }
 
-fn all_department_print(hashmap: HashMap<String, Vec<String>>, target_department: Vec<&str>) {
+fn all_department_print(company_department_map: HashMap<String, Vec<String>>, target_department: Vec<&str>) {
     for i in 0..target_department.len() {  
         let index = i;
-        department_print(hashmap.clone(), target_department.clone(), index.try_into().unwrap());
+        department_print(company_department_map.clone(), target_department.clone(), index.try_into().unwrap());
     }
 }
 
-// fn manual_entry(v: &mut Vec<String>) {
-//     println!("Please enter your selection: 'X' to Exit");
+fn manual_entry(v: &mut Vec<String>) {
+    println!("Please enter your selection: 'X' to Exit");
     
-//     let mut user_entry = String::new();
-//     let mut clean_float = Vec::new();
+    let mut user_entry = String::new();
+    let mut clean_float = Vec::new();
     
-//     io::stdin()
-//         .read_line(&mut user_entry)
-//         .expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut user_entry)
+        .expect("Failed to read line");
     
-//     user_entry = user_entry.trim().to_string();
+    user_entry = user_entry.trim().to_string();
 
-//     for char in user_entry.chars() {
-//         if char.is_ascii(){
-//             clean_float.push(char as u8)
-//         }
-//     }
+    for char in user_entry.chars() {
+        if char.is_ascii(){
+            clean_float.push(char as u8)
+        }
+    }
     
-//     if !clean_float.is_empty() {
-//         v.push(String::from_utf8(clean_float).expect("Our bytes should be valid utf8"));
-//     }
+    if !clean_float.is_empty() {
+        v.push(String::from_utf8(clean_float).expect("Our bytes should be valid utf8"));
+    }
 
-//     for char in user_entry.chars() {
-//         if char == 'x' || char == 'X'{
-//             println!("Exiting...");
-//         }
-//     }
-// }
+    for char in user_entry.chars() {
+        if char == 'x' || char == 'X'{
+            println!("Exiting...");
+        }
+    }
+}
